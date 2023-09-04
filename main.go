b93ac2175b751c54c92d06296ef0f4eedcd0aebf
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-04 14:37:33 +0300

    setup cors configuration

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/AkifhanIlgaz/wit-api/controllers"
 	"github.com/AkifhanIlgaz/wit-api/firebase"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -32,6 +33,20 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Use(uidMiddleware.SetUid)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Successfully send request to localhost")
+	})
 
 	r.Route("/outfits", func(r chi.Router) {
 		r.Get("/all/{uid}", outfitsController.GetAllOutfitsByUid)
